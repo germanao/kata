315db81813930a4841f99521a9e3bfdffde02496
Germano Benini <germano_benini@hotmail.com>
2024-07-30 20:22:18 -0300

    Init project

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Kata.Metodo();
+
+public class Kata {
+
+    public static int Fib(int n)
+    {
+           if (n==1||n==2)
+            {
+                return 1;
+            }
+            return Fib(n - 1) + Fib(n - 2);
+    }
+
+    public static void Metodo(){
+        Console.WriteLine(Fib(0));
+    }
+}
+
+
+ 
